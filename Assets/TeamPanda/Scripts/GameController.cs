d4928e5c444080b12d819fa78cb95195f6799efb
Felix Gruner <felix.gruner@gmail.com>
2015-07-22 19:09:29 +0200

    added game controller

---
+++ Assets/TeamPanda/Scripts/GameController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// TODO initialize stuff
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FuseSpirits(SpiritStateController spirit1, SpiritStateController spirit2) {
+		// TODO get level of both spirits, get collision position, create one next level spirit at collision position
+	}
+}
