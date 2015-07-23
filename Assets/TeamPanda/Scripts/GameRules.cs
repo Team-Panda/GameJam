c296ea17a42bedd3677e777fc149e37ea2b10111
Felix Gruner <felix.gruner@gmail.com>
2015-07-23 13:38:29 +0200

    player visualization- colors and size on level up

--- Assets/TeamPanda/Scripts/GameRules.cs
+++ Assets/TeamPanda/Scripts/GameRules.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameRules : MonoBehaviour {
 
@@ -10,7 +11,23 @@ public class GameRules : MonoBehaviour {
 	// required collections points for level transitions
 	private static int[] levelConditions = new int[] {1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233};
 
+	// color codes according to level
+	private static Color[] levelColors = new Color[] {
+		new Color(9/255f,117/255f,130/255f,1),
+		new Color(226/255f, 134/255f, 119/255f,1),
+		new Color(86/255f, 212/255f, 191/255f,1),
+		new Color(255/255f, 168/255f, 9/255f,1),
+		new Color(190/255f, 126/255f, 227/255f,1),
+		new Color(139/255f, 241/255f, 245/255f,1),
+		new Color(255/255f, 237/255f, 101/255f,1),
+		new Color(255/255f, 107/255f, 95/255f,1),
+		new Color(174/255f, 63/255f, 141/255f,1),
+		new Color(208/255f, 188/255f, 155/255f,1)
+	};
+
+
 	// public getters
 	public static int MaxLevel { get{ return maxLevel; } }
 	public static int[] LevelConditions { get{ return levelConditions; } }
+	public static Color[] LevelColors { get{ return levelColors; } }
 }
--- Assets/TeamPanda/Scripts/PlayerStateVisualizer.cs
+++ Assets/TeamPanda/Scripts/PlayerStateVisualizer.cs
@@ -9,10 +9,19 @@ public class PlayerStateVisualizer : MonoBehaviour {
 	private Vector3 startScale;
 	private Vector3 scaleIncrease;
 
+	private SkinnedMeshRenderer rend;
+	private GameObject model;
+
 	// Use this for initialization
 	void Start () {
 		startScale = new Vector3 (StartScale, StartScale, StartScale);
 		scaleIncrease = new Vector3 (ScaleIncrease, ScaleIncrease, ScaleIncrease);
+
+		model = GameObject.FindWithTag ("PlayerModel");
+		rend = model.GetComponent<SkinnedMeshRenderer> ();
+		//Debug.Log (GameRules.LevelColors [0] +" ..... "+Color.red);
+		setColor(GameRules.LevelColors [0]);
+
 		// set start scale
 		transform.localScale = startScale;
 	}
@@ -22,9 +31,17 @@ public class PlayerStateVisualizer : MonoBehaviour {
 	
 	}
 
+	private void setColor(Color col) {
+		rend.material.color = col;
+	}
+
 	public void setLevelUp(int currentLevel) {
 		// TODO level up animation?
 
+	
+		// set color according to level
+		Color newColor = GameRules.LevelColors [currentLevel];
+		setColor(newColor);
 
 		transform.localScale = startScale + scaleIncrease * currentLevel;
 	}
